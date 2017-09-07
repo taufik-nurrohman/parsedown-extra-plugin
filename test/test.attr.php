71a96080909522f712d0a1530e4b1d1576ceaef8
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-09-07 15:40:21 +0700

    Allow Colon in Fragment Identifier

--- test/test.attr.php
+++ test/test.attr.php
@@ -4,6 +4,8 @@ $s = <<<S
 
 Lorem ipsum dolor [sit](foo) {.foo.bar#baz#qux} amet.
 
+Lorem ipsum dolor [sit](foo) {#foo:bar .baz:qux} amet.
+
 Lorem ipsum dolor [sit](foo) {a} amet.
 
 Lorem ipsum dolor [sit](foo) {a=b} amet.
@@ -21,6 +23,36 @@ Lorem ipsum dolor [sit](foo) {a=''} amet.
 ---
 
 Lorem ipsum dolor [sit](foo) {a="b" c=d e f="" g= h} amet.
+
+Lorem ipsum dolor [sit](foo) {a="b\"b\'b" b='c\'c\"c' c=d e f="" g= h} amet.
+
+---
+
+Lorem ipsum dolor [_sit_](foo) {target="_blank"} amet.
+
+Lorem ipsum dolor [_sit_](foo) {target='_blank'} amet.
+
+Lorem ipsum dolor [_sit_](foo) {target=_blank} amet.
+
+Lorem ipsum dolor [*sit*](foo) {foo="*bar"} amet.
+
+Lorem ipsum dolor [*sit*](foo) {foo='*bar'} amet.
+
+Lorem ipsum dolor [*sit*](foo) {foo=*bar} amet.
+
+### Issue #4
+
+Lorem ipsum dolor _[sit](foo) {target="_blank"}_ amet.
+
+Lorem ipsum dolor _[sit](foo) {target='_blank'}_ amet.
+
+Lorem ipsum dolor _[sit](foo) {target=_blank}_ amet.
+
+Lorem ipsum dolor *[sit](foo) {foo="*bar"}* amet.
+
+Lorem ipsum dolor *[sit](foo) {foo='*bar'}* amet.
+
+Lorem ipsum dolor *[sit](foo) {foo=*bar}* amet.
 
 S;
 
